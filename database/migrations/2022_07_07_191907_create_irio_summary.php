0b2c1338dc2ae7df4951c61019e9f8929c47a041
Eko Putra <52368104+ekoptra@users.noreply.github.com>
2022-07-08 02:59:43 +0700

    Buat migration dan seeder untuk irio summary table (#2)
    * create migration irio summary
    
    * make seeder for irio summary table
    
    * mend

---
+++ database/migrations/2022_07_07_191907_create_irio_summary.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('irio_summary', function (Blueprint $table) {
+            $table->id();
+            $table->char('kode_provinsi', 2);
+            $table->string('provinsi');
+            $table->string('kode_kategori', 10);
+            $table->string('kategori');
+            $table->double('latitude')->nullable();
+            $table->double('longitude')->nullable();
+            $table->string('variable');
+            $table->double('value')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('irio_summary');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\IrioSummary;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,14 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
         $this->call([
-            AdminSeeder::class
+            AdminSeeder::class,
+            IrioSummary::class
         ]);
     }
 }
