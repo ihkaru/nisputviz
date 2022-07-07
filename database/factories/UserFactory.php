250463f989f299bab3cb1ddac5939a55ff0c845c
ihkaru <221810336@stis.ac.id>
2022-07-07 15:03:04 +0700

    add seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$MX1nPYqvxEuaogE.mLYP3.TOTUW9cmKHrqy5Qwq/A37yhVjO4.d3C', // password
             'remember_token' => Str::random(10),
         ];
     }
