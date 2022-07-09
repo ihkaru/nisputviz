32e93a4c267908a85df20dbbf66d7e49928243c1
fikrianggara <221810306@stis.ac.id>
2022-07-10 00:45:28 +0700

    add tableau visualization and home page

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,1 +1,5 @@
-<h1>Aku Footer</h1>
+
+        <div><span>Sumber data :</span><a
+                href="https://www.bps.go.id/publication.html?Publikasi%5BtahunJudul%5D=&Publikasi%5BkataKunci%5D=input+output&Publikasi%5BcekJudul%5D=0&yt0=Tampilkan"
+                class="text-orange-600 hover:cursor-pointer"> BPS RI</a></div>
+
