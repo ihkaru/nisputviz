30e76c6072979f08b1c4f1a166feb02e8cf09299
Eko Putra <52368104+ekoptra@users.noreply.github.com>
2022-07-10 00:35:51 +0700

    Data dashboard 2 (#13)
    * create migration and model
    
    * add resource to update link tableau
    
    * add jenis dashboard
    
    * create migration
    
    * add seeder and model

---
+++ app/Models/IrioIntraInter.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IrioIntraInter extends Model
+{
+    use HasFactory;
+
+    protected $table = 'irio_intra_inter';
+    protected $guarded = [];
+}
